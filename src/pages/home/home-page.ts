84799130e8b7a49c882b5be68059fa069d43a4b9
Ismet <kadyrovismet1999@gmail.com>
2026-09-21 04:49:16 +0500

    feat: add hero section layout

--- src/pages/home/home-page.ts
+++ src/pages/home/home-page.ts
@@ -1,9 +1,11 @@
+import { createHero } from '@/components/hero/hero';
 import { createElement } from '@/shared/lib/dom';
 import type { Component } from '@/shared/types/component';
 import './home-page.scss';
 
 export function createHomePage(): Component {
-  const element = createElement('div', { className: 'home-page', text: 'home-page' });
+  const hero = createHero();
+  const element = createElement('div', { className: 'home-page', children: [hero.element] });
 
   return { element };
 }
