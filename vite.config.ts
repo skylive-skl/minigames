a77c4b507f317f59b46a1a843227d07cbbde6ddf
Ismet <kadyrovismet1999@gmail.com>
2026-09-21 01:04:11 +0500

    build: add husky hooks for commit-msg and pre-push

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ export default defineConfig(({ mode }) => ({
   base: '/minigames/',
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
+      '@': fileURLToPath(new URL('src', import.meta.url)),
     },
   },
   build: {
